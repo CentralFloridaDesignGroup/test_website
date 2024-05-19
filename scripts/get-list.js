c2592604e9ccd731033907ac5c4fedadf58ce1ff
Nathan White <57015867+NWhite421@users.noreply.github.com>
2024-05-19 15:25:50 -0400

    Add files via upload

--- scripts/get-list.js
+++ scripts/get-list.js
@@ -1,6 +1,29 @@
-$.getJSON("scripts/atwell_drones.json", function(json) {
+$.getJSON("scripts/atwell_drones.json", function (json) {
   json.forEach(element => {
     console.log(element);
     document.getElementById("Drone_Name").innerHTML += `<option>${element.id} - ${element.name}</option>`
   });
 });
+
+$
+
+function initGeolocation() {
+  if (navigator.geolocation) {
+    // Call getCurrentPosition with success and failure callbacks
+    navigator.geolocation.getCurrentPosition(success, fail);
+  }
+  else {
+    alert("Sorry, your browser does not support geolocation services.");
+  }
+}
+
+function success(position) {
+
+  document.getElementById('Report-Longitude').value = position.coords.longitude;
+  document.getElementById('Report-Latitude').value = position.coords.latitude
+}
+
+function fail() {
+  // Could not obtain location
+}
+
